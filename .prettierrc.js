e7a649955a6d2c5b918220968b1207af5719ad54
Justin <l6104400@gmail.com>
2021-08-20 00:22:22 +0800

    build: add basic router config

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  arrowParens: 'avoid',
+  pugSingleQuote: false,
+  pugPrintWidth: 100,
+  pugAttributeSeparator: 'none',
+  pugClosingBracketPosition: 'last-line',
+  pugSortAttributes: 'asc',
+};
--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { Switch, Route, Link } from 'react-router-dom';
+
 import './App.css';
+import Home from './modules/home/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
