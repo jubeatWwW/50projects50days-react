5bc10b84bff609d3505981d7ce7c20a1b1311cb4
Justin <l6104400@gmail.com>
2021-08-20 01:01:46 +0800

    feat: add expanding cards component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Switch, Route, Link } from 'react-router-dom';
 
 import './App.css';
 import Home from './modules/home/home';
+import ExpandingCards from './modules/expanding-cards/expanding-cards';
 
 function App() {
   return (
@@ -10,8 +11,14 @@ function App() {
         <li>
           <Link to="/">Home</Link>
         </li>
+        <li>
+          <Link to="/projects/expanding-cards">Expanding Cards</Link>
+        </li>
       </ul>
       <Switch>
+        <Route path="/projects/expanding-cards">
+          <ExpandingCards />
+        </Route>
         <Route path="/">
           <Home />
         </Route>
